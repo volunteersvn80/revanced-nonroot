991dae110bdd37464a35d2085c3bf251c1568c7f
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-06-03 00:31:12 +0700

    Update build-rv.sh

--- src/build-rv.sh
+++ src/build-rv.sh
@@ -26,35 +26,35 @@ dl_gh "revanced" "revanced-patches revanced-cli revanced-integrations" "latest"
 
 # Messenger
 get_patches_key "messenger"
-#get_apkmirror "messenger" "messenger" "facebook-2/messenger/messenger" "arm64-v8a"
+#get_apkmirror "messenger" "arm64-v8a"
 get_uptodown "messenger" "facebook-messenger"
 patch "messenger" "messenger-revanced"
 
 # Patch Twitch 
 get_patches_key "twitch"
 get_ver "block-video-ads" "tv.twitch.android.app"
-get_apkmirror "twitch" "twitch" "twitch-interactive-inc/twitch/twitch"
+get_apkmirror "twitch"
 #get_uptodown "twitch" "twitch"
 patch "twitch" "twitch-revanced"
 
 # Patch Tiktok 
 get_patches_key "tiktok"
 #get_ver "feed-filter" "com.ss.android.ugc.trill"
-get_apkmirror "tiktok" "tik-tok-including-musical-ly" "tiktok-pte-ltd/tik-tok-including-musical-ly/tik-tok-including-musical-ly"
+get_apkmirror "tiktok"
 #get_uptodown "tiktok" "tik-tok"
 patch "tiktok" "tiktok-revanced"
 
 # Patch YouTube 
 get_patches_key "youtube-revanced"
 get_ver "video-ads" "com.google.android.youtube"
-get_apkmirror "youtube" "youtube" "google-inc/youtube/youtube"
+get_apkmirror "youtube"
 #get_uptodown "youtube" "youtube" 
 patch "youtube" "youtube-revanced"
 
 # Patch YouTube Music 
 get_patches_key "youtube-music-revanced"
 get_ver "hide-get-premium" "com.google.android.apps.youtube.music"
-get_apkmirror "youtube-music" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
+get_apkmirror "youtube-music" "arm64-v8a"
 #get_uptodown "youtube-music" "youtube-music" 
 patch "youtube-music" "youtube-music-revanced"
 

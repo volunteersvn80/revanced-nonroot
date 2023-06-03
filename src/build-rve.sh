aa3de2a27c4c5f1b3d42a7f149f65174c5d996a4
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-06-03 13:04:46 +0700

    Update build-rve.sh

--- src/build-rve.sh
+++ src/build-rve.sh
@@ -28,13 +28,13 @@ dl_gh "inotia00" "revanced-patches revanced-cli revanced-integrations" "latest"
 get_patches_key "youtube-revanced-extended"
 get_ver "hide-general-ads" "com.google.android.youtube"
 get_apkmirror "youtube"
-#get_uptodown "youtube" "youtube"
+#get_uptodown "youtube"
 patch "youtube" "youtube-revanced-extended"
 
 # Patch YouTube Music Extended 
 get_patches_key "youtube-music-revanced-extended"
 #get_apkmirror "youtube-music" "arm64-v8a"
-get_uptodown "youtube-music" "youtube-music" 
+get_uptodown "youtube-music" 
 patch "youtube-music" "youtube-music-revanced-extended"
 
 # Patch microG

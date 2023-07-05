2725a456817f6e7d1cbd288b3058761dd453b923
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-07-05 21:37:52 +0700

    Update build-rve.sh

--- src/build-rve.sh
+++ src/build-rve.sh
@@ -17,7 +17,6 @@ patch "youtube" "youtube-revanced-extended-v$version"
 
 # Patch YouTube Music Extended 
 get_patches_key "youtube-music-revanced-extended"
-version="6.04.53"
 #get_apkmirror "youtube-music" "arm64-v8a"
 get_uptodown "youtube-music" 
 patch "youtube-music" "youtube-music-revanced-extended-v$version"

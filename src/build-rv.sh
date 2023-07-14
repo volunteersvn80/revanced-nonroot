d9a052586ed4fee89486c91db8e37cb2372b949e
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-07-14 11:02:51 +0700

    Update build-rv.sh

--- src/build-rv.sh
+++ src/build-rv.sh
@@ -11,44 +11,51 @@ dl_gh "revanced" "revanced-patches revanced-cli revanced-integrations" "latest"
 # Use v$version to take apk with version
 # Example: patch "reddit" "reddit-revanced-v$version"
 
+# Twitter
+get_patch_key "twitter"
+get_ver "twitter"
+get_apkmirror "twitter"
+#get_uptodown "twitter"
+patch "twitter" "twitter-revanced-$version"
+
 # Reddit
-get_patches_key "reddit"
-get_apkmirror "reddit"
+#get_patches_key "reddit"
+#get_apkmirror "reddit"
 #get_uptodown "reddit"
-patch "reddit" "reddit-revanced-v$version"
+#patch "reddit" "reddit-revanced-v$version"
 
 # Messenger
-get_patches_key "messenger"
+#get_patches_key "messenger"
 #get_apkmirror "messenger" "arm64-v8a"
-get_uptodown "messenger"
-patch "messenger" "messenger-revanced-v$version"
+#get_uptodown "messenger"
+#patch "messenger" "messenger-revanced-v$version"
 
 # Patch Twitch 
-get_patches_key "twitch"
-get_ver "twitch"
-get_apkmirror "twitch"
+#get_patches_key "twitch"
+#get_ver "twitch"
+#get_apkmirror "twitch"
 #get_uptodown "twitch"
-patch "twitch" "twitch-revanced-v$version"
+#patch "twitch" "twitch-revanced-v$version"
 
 # Patch Tiktok Asia or Global. Keyword patch is the same get Apk
-get_patches_key "tiktok"
+#get_patches_key "tiktok"
 #get_apkmirror "tiktok"
-get_uptodown "tiktok-asia"
+#get_uptodown "tiktok-asia"
 #get_uptodown "tiktok-global
-patch "tiktok-asia" "tiktok-asia-revanced-v$version"
+#patch "tiktok-asia" "tiktok-asia-revanced-v$version"
 
 # Patch YouTube 
-get_patches_key "youtube-revanced"
-get_ver "youtube-rv"
-get_apkmirror "youtube"
+#get_patches_key "youtube-revanced"
+#get_ver "youtube-rv"
+#get_apkmirror "youtube"
 #get_uptodown "youtube"
-patch "youtube" "youtube-revanced-v$version"
+#patch "youtube" "youtube-revanced-v$version"
 
 # Patch YouTube Music 
-get_patches_key "youtube-music-revanced"
+#get_patches_key "youtube-music-revanced"
 #get_apkmirror "youtube-music" "arm64-v8a"
-get_uptodown "youtube-music"
-patch "youtube-music" "youtube-music-revanced-v$version"
+#get_uptodown "youtube-music"
+#patch "youtube-music" "youtube-music-revanced-v$version"
 
 # Finish patch
 finish_patch "revanced"

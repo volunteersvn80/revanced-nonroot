3153c39b7e541d83d98e47a710bd5772e67bda6a
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2023-07-30 23:45:36 +0700

    Update build-rv.sh

--- src/build-rv.sh
+++ src/build-rv.sh
@@ -6,8 +6,7 @@ source ./src/tools.sh
 check_new_patch "revanced" "revanced"
 
 #Download Revanced patches
-dl_gh "revanced" "revanced-patches revanced-integrations" "latest"
-dl_gh "j-hc" "revanced-cli" "latest"
+dl_gh "revanced" "revanced-patches revanced-cli revanced-integrations" "latest"
 
 # Use v$version to take apk with version
 # Example: patch "reddit" "reddit-revanced-v$version"
